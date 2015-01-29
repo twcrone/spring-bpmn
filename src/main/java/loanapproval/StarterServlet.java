25d04050d97c2bc65e47529e524a1268bd5aa391
Todd Crone <todd.crone@perceptivesoftware.com>
2015-01-29 11:24:11 -0500

    Make servlet run process

--- src/main/java/loanapproval/StarterServlet.java
+++ src/main/java/loanapproval/StarterServlet.java
@@ -1,5 +1,9 @@
 package loanapproval;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,9 +11,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class StarterServlet extends HttpServlet {
+
+    ApplicationContext context = null;
+
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        if(context == null) {
+            context = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+        }
+
+        RuntimeService runtimeService = (RuntimeService) context.getBean("runtimeService");
+        runtimeService.startProcessInstanceByKey("loanApproval");
+
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<html><body>Hello</body></html>");
+        out.println("<html><body>Done</body></html>");
     }
 }
